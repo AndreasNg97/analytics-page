b78338d0d6cb830fade24b55917b467080622abe
AndreasNg97 <54266373+AndreasNg97@users.noreply.github.com>
2020-11-30 14:39:29 +0100

    Updates

---
+++ precache-manifest.1c92c3484b3f0f8beda68746573833a8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d42510b0bee1dfd17f3d5178fa3f3be",
+    "url": "/analytics-page/index.html"
+  },
+  {
+    "revision": "2b405c9df8b05e8f796c",
+    "url": "/analytics-page/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9a296975538736f54d89",
+    "url": "/analytics-page/static/css/main.6f324761.chunk.css"
+  },
+  {
+    "revision": "2b405c9df8b05e8f796c",
+    "url": "/analytics-page/static/js/2.e54f5e50.chunk.js"
+  },
+  {
+    "revision": "8b81bc38395cca664a526c4e304a6603",
+    "url": "/analytics-page/static/js/2.e54f5e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a296975538736f54d89",
+    "url": "/analytics-page/static/js/main.6077d47c.chunk.js"
+  },
+  {
+    "revision": "3b50bcaac65e420a9f2e",
+    "url": "/analytics-page/static/js/runtime-main.ddd67d54.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/analytics-page/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "ae3b850d3afb7bf21234e29fd7be7137",
+    "url": "/analytics-page/static/media/avatar@2x.ae3b850d.png"
+  },
+  {
+    "revision": "527b95555f82698d08701a08eb54a750",
+    "url": "/analytics-page/static/media/funds.527b9555.svg"
+  },
+  {
+    "revision": "116b955510aeb395b219eb31eb03be5f",
+    "url": "/analytics-page/static/media/group.116b9555.svg"
+  },
+  {
+    "revision": "d59a05e9cf16c596b9ae94740093c640",
+    "url": "/analytics-page/static/media/history.d59a05e9.svg"
+  },
+  {
+    "revision": "46a1f33ee3a375cad830f6e0c9f300ee",
+    "url": "/analytics-page/static/media/locked-padlock.46a1f33e.svg"
+  },
+  {
+    "revision": "8a1cdf8c9d83437bed69dab5c8f765c3",
+    "url": "/analytics-page/static/media/path.8a1cdf8c.svg"
+  },
+  {
+    "revision": "5237004efb0aae4c6a05cc2dc05252a9",
+    "url": "/analytics-page/static/media/verification-window-button.5237004e.svg"
+  }
+]);
